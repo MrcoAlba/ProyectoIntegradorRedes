f7d18c68c3973e19fceb446fd426dcf03f42cf2d
MrcoAlba <65100651+MrcoAlba@users.noreply.github.com>
2021-02-03 14:23:36 -0500

    Avance de comentarios (Interfaz-Router) Marco

--- src/paquete/clases/Interfaz.java
+++ src/paquete/clases/Interfaz.java
@@ -1,45 +1,45 @@
-
 package paquete.clases;
 
 public class Interfaz {
     public String red, ip, mascara, nombre;
-
+    // String red -> Creada con la finalidad de guardar la ip de la red en la que se encuentra conectada la interfaz. Ejm: red *192.168.1.0*
+    // String ip -> Creada con la finalidad de guardar la ip perteneciente a la interfaz con respecto a su red. Ejm: ip *192.168.1.1*
+    // String mascara -> Creada con la finalidad de guardar la máscara de ip según corresponda. Ejm: mascara de red *255.255.255.0*
+    // String nombre -> Creada con la finalidad de almacenar por medio de un nombre sencillo el pseudonimo de la red. Ejm: red de nombre = *gig0/1*
+    
+    
+    //Se crea un método constructor.
     public Interfaz(String red, String ip, String mascara, String nombre) {
         this.red = red;
         this.ip = ip;
         this.mascara = mascara;
         this.nombre = nombre;
     }
-
-    public String getRed() {
+    
+    
+    //Se crean los getters y setters de la clase Interfaz que luego serán utilizados en la clase GestionRedes
+    public String   getRed() {
         return red;
     }
-
-    public void setRed(String red) {
+    public void     setRed(String red) {
         this.red = red;
     }
-
-    public String getIp() {
+    public String   getIp() {
         return ip;
     }
-
-    public void setIp(String ip) {
+    public void     setIp(String ip) {
         this.ip = ip;
     }
-
-    public String getMascara() {
+    public String   getMascara() {
         return mascara;
     }
-
-    public void setMascara(String mascara) {
+    public void     setMascara(String mascara) {
         this.mascara = mascara;
     }
-
-    public String getNombre() {
+    public String   getNombre() {
         return nombre;
     }
-
-    public void setNombre(String nombre) {
+    public void     setNombre(String nombre) {
         this.nombre = nombre;
     }
     
--- src/paquete/clases/Router.java
+++ src/paquete/clases/Router.java
@@ -13,87 +13,90 @@ public class Router{
     public String [] RedesR;
     public int [] hopV;
     public int [] hopR;
-
+    // ArrayList<Interfaz> interfaces -> Creada con la finalidad de almacenar un numero variable de interfaces por router.
+    // int contRedesV=0 -> Creado con la finalidad de contar el numero de redes VECINAS que tiene el router. Se establece 0 y aumenta al añadir una interfaz.
+    // int contRedesR=0 -> Creado con la finalidad de contar el numero de redes REMOTAS que tiene el router. El número aumenta en la clase gestion.
+    // String nickname -> Creada con la finalidad de tener un pseudonido del router. Ejms: R1, R2, ...
+    // String [] RedesV -> Creada con la finalidad de almacenar las redes VECINAS existentes en la topologia.
+    // String [] RedesR -> Creada con la finalidad de almacenar las redes REMOTAS existentes en la topologia.
+    // int [] hopV -> Creado con la finalidad de saber cuantos saltos (hops) se necesita dar para llegar a la red VECINA. Siempre es uno por teoria.
+    // int [] hopR -> Creado con la finalidad de saber cuantos saltos (hops) se necesita dar para llegar a una red REMOTA. El numero es variable y se genera en la clase GestionRedes.
+    
+    
+    //Se crea un metodo constructor.
     public Router(String nickname) {
         this.nickname = nickname;
-        RedesV = new String[10];
-        RedesR = new String[30];
-        hopV = new int[10];
-        hopR = new int[30];
+        RedesV = new String[10];    //Se establece el numero de redes vecinas de 10 a pesar de que los routers solo ofrezcan un maximo de 3 interfaces de conexion.
+        RedesR = new String[30];    //Se establece el numero de redes remotas a 30, a pesar de que el algoritmo Vector-Distancia (aplicado en RIPv2) no permita mas de 15 nodos por topologia.
+        hopV = new int[10];         //Se establece un arreglo de 10 debido a la explicación previa
+        hopR = new int[30];         //Se establece un arreglo de 30 debido a la explicación previa
         interfaces = new ArrayList<Interfaz>();
     }
-
-    public String getNickname() {
+    
+    
+    //Se establecen los gettes y settes de la clase.
+    public String   getNickname() {
         return nickname;
     }
-
-    public void setNickname(String nickname) {
+    public void     setNickname(String nickname) {
         this.nickname = nickname;
     }
-
     public String[] getRedesV() {
         return RedesV;
     }
-
-    public void setRedesV(String[] RedesV) {
+    public void     setRedesV(String[] RedesV) {
         this.RedesV = RedesV;
     }
-    
     public String[] getRedesR() {
         return RedesR;
     }
-
-    public void setRedes(String[] RedesR) {
+    public void     setRedes(String[] RedesR) {
         this.RedesR = RedesR;
     }
-    
-    public int getContRedesV() {
+    public int      getContRedesV() {
         return contRedesV;
     }
-
-    public void setContRedesV(int contRedesV) {
+    public void     setContRedesV(int contRedesV) {
         this.contRedesV = contRedesV;
     }
-    
-    public int getContRedesR() {
+    public int      getContRedesR() {
         return contRedesR;
     }
-
-    public void setContRedesR(int contRedesR) {
+    public void     setContRedesR(int contRedesR) {
         this.contRedesR = contRedesR;
     }
-    
-    public int[] getHopV() {
+    public int[]    getHopV() {
         return hopV;
     }
-
-    public void setHopV(int[] hopV) {
+    public void     setHopV(int[] hopV) {
         this.hopV = hopV;
     }
-
-    public int[] getHopR() {
+    public int[]    getHopR() {
         return hopR;
     }
-
-    public void setHopR(int[] hopR) {
+    public void     setHopR(int[] hopR) {
         this.hopR = hopR;
     }
     
+    
+    //Se incluye el metodo Adicionar para agregar objetos de la clase Interfaz
     public void Adicionar(Interfaz obj)
     {
         interfaces.add(obj);
     }
     
+    
+    //Se incluye el metodo GuardarRedes que sigue el siguiente algoritmo:
     public void GuardarRedes()
     {
-        int i=0;
-        Iterator<Interfaz> puntero = interfaces.iterator();
-        while(puntero.hasNext())
+        int i=0;                                                        //Se establece un contador que inicia en 0
+        Iterator<Interfaz> puntero = interfaces.iterator();             //Se crea un iterador que recorrerá todo el arreglo
+        while(puntero.hasNext())                                        //Mientras hayan redes en el arreglo (recordar que las redes fueron agregadas arriba)
         {
-            Interfaz a = puntero.next();
-            RedesV[i]=a.getRed();
-            hopV[i]=1;
-            i++;
+            Interfaz a = puntero.next();                                //Se almacenaran los datos de la interfaz almacenada en una variable de tipo Interfaz
+            RedesV[i]=a.getRed();                                       //Se guardará los datos de *red* de la interfaz en el *arreglo de redes vecinas*.
+            hopV[i]=1;                                                  //Se establecerá 1 en todas las redes vecinas (Explicado por teoria de V-D)
+            i++;                                                        //Se aumentará el contador para almacenar los siguientes datos en el siguiente espacio de RedesV y hopV
         }
         this.contRedesV=i;
     }
